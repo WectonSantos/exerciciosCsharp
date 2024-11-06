1a5cb795da16ccddb7fd8e4b3d0b2227b455935e
Wecton <Wecton.Santos12@outlook.com>
2024-11-06 09:50:34 -0300

    tp 11 inc

---
+++ Atividades/Atividade 11/tp11-wecton-228550/tp11-wecton-228550/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace tp11_wecton_228550
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCartao = new System.Windows.Forms.Label();
+            this.txtCartao = new System.Windows.Forms.TextBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblCartao
+            // 
+            this.lblCartao.AutoSize = true;
+            this.lblCartao.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCartao.Location = new System.Drawing.Point(7, 9);
+            this.lblCartao.Name = "lblCartao";
+            this.lblCartao.Size = new System.Drawing.Size(208, 24);
+            this.lblCartao.TabIndex = 0;
+            this.lblCartao.Text = "Digite o nº do cartão:";
+            // 
+            // txtCartao
+            // 
+            this.txtCartao.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCartao.Location = new System.Drawing.Point(224, 5);
+            this.txtCartao.Name = "txtCartao";
+            this.txtCartao.Size = new System.Drawing.Size(261, 32);
+            this.txtCartao.TabIndex = 1;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerificar.Location = new System.Drawing.Point(502, 5);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(102, 32);
+            this.btnVerificar.TabIndex = 2;
+            this.btnVerificar.Text = "VERIFICAR";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(219, 47);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(66, 25);
+            this.lblResultado.TabIndex = 3;
+            this.lblResultado.Text = "Status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 77);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.txtCartao);
+            this.Controls.Add(this.lblCartao);
+            this.Name = "Form1";
+            this.Text = "Validação de Cartão";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCartao;
+        private System.Windows.Forms.TextBox txtCartao;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
